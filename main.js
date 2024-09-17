533983c778b6499902db874d4edf516a3a12c0fc
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-17 22:49:34 +0500

    javohirniki

---
+++ main.js
@@ -0,0 +1,44 @@
+for (let item = 0; item <= 20; item = item + 1 * 2) {
+  console.log(item);
+}
+console.log("2 karra javoblari");
+
+for (let number = 0; number <= 30; number = number + 1 * 3) {
+  console.log(number);
+}
+console.log("3 karra javoblari");
+
+for (let result = 0; result <= 40; result = result + 1 * 4) {
+  console.log(result);
+}
+console.log("4 karra javoblari");
+
+for (let car = 0; car <= 50; car = car + 1 * 5) {
+  console.log(car);
+}
+console.log("5 karra javoblari");
+
+for (let lup = 0; lup <= 60; lup = lup + 1 * 6) {
+  console.log(lup);
+}
+console.log("6 karra javoblari");
+
+for (let git = 0; git <= 70; git = git + 1 * 7) {
+  console.log(git);
+}
+console.log("7 karra javoblari");
+
+for (let hub = 0; hub <= 80; hub = hub + 1 * 8) {
+  console.log(hub);
+}
+console.log("8 karra javoblari");
+
+for (let laptop = 0; laptop <= 90; laptop = laptop + 1 * 9) {
+  console.log(laptop);
+}
+console.log("9 karra javoblari");
+
+for (let ten = 0; ten <= 100; ten = ten + 1 * 10) {
+  console.log(ten);
+}
+console.log("10 karra javoblari");
